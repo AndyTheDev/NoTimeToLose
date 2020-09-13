391cd9a0f897e7dd1b4498c241e400a82266d9f6
Andrej Gončarov <a.goncarov@outlook.cz>
2020-09-13 13:10:50 +0200

    Puppies Litter A Update

--- form_process.php
+++ form_process.php
@@ -5,11 +5,11 @@
     $admin_email = "vipet@notimetolose.eu";
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
+    $telefon = $_POST['telefon'];
     $message = $_POST['message'];
     
     //send email
-    mail($admin_email, "No Time To Lose:" . $subject, $message, "From:" . $email);
+    mail($admin_email, "No Time To Lose:" . $telefon, $message, "From:" . $email);
     
     header('Location: http://www.notimetolose.eu/form_succes.html');
   }
